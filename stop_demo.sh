3629096c7a41a3e47e701cfaf7f2998082d7171b
Phillip Booth <phillip.booth2015@gmail.com>
2020-11-19 12:26:55 -0500

    cadding .env file to ensure network is named correctly

---
+++ stop_demo.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#
+# This script is meant to support running the demo with Hyperledger
+#
+
+
+YELLOW="\033[1;33m"
+BLUE="\033[1;34m"
+PURPLE="\033[1;35m"
+CYAN="\033[1;36m"
+WHITE="\033[1;37m"
+RESET="\033[0m"
+
+printf "\n\n${PURPLE}Stopping Demo and Cleaning Up Test Network Configurations.${RESET}\n"
+docker-compose down --volumes --remove-orphans
+
+docker-compose -f docker-compose-ca.yml down --volumes --remove-orphans
+
+docker run --rm --name hyperledgerw \
+--net host \
+-v `pwd`/hyperledger/organizations:/hyperledger/fabric-samples/test-network/organizations \
+-v `pwd`/hyperledger/system-genesis-block:/hyperledger/fabric-samples/test-network/system-genesis-block \
+intersystemsdc/irisdemo-base-hyperledgerw:version-latest cleanUp
